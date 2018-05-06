1033fd2dfa1a708b9cf04f7066fb617585dc2fbe
Drew Seward <awswrd@gmail.com>
2018-05-05 17:26:10 -0700

    wip: Feeling out reviewers on PR webhooks

--- lib/code_coordinator_web/controllers/webhook_controller.ex
+++ lib/code_coordinator_web/controllers/webhook_controller.ex
@@ -3,21 +3,45 @@ require IEx
 defmodule CodeCoordinatorWeb.WebhookController do
   use CodeCoordinatorWeb, :controller
 
-  def handle(conn, _params) do
+  def handle(conn, params) do
     case try_recognize_event(conn) do
-      {:ok, _event_type} ->
-        send_resp(conn, 200, "")
-      {:error, msg} ->
-        send_resp(conn, 400, msg)
+      {:ok, event_type} ->
+        try_handle(event_type, params)
+        conn |> send_resp(200, "")
+
+      {:error, msg} -> conn |> send_resp(400, msg)
     end
   end
 
   defp try_recognize_event(conn) do
     case get_req_header(conn, "x-github-event") do
-      [event_type] when event_type in ["pull_request", "pull_request_review_comment", "pull_request_review"] ->
+      [event_type] when event_type in ["ping", "pull_request", "pull_request_review_comment", "pull_request_review"] ->
         {:ok, event_type}
       _ ->
         {:error, "Invalid event_type"}
     end
+  end
+
+  defp try_handle(event_type, params) do
+    case event_type do
+      "ping" -> nil
+      "pull_request" -> handle_pull_request(params)
+      "pull_request_review_comment" -> handle_pull_request_review_comment(params)
+      "pull_request_review" -> handle_pull_request_review(params)
+    end
+  end
+
+  defp handle_pull_request(params) do
+    %{"action" => action} = params
+    if action == "review_requested" do
+      IEx.pry
+    end
+    IO.puts "Action: #{action}"
+  end
+
+  defp handle_pull_request_review_comment(payload) do
+  end
+
+  defp handle_pull_request_review(payload) do
   end
 end
